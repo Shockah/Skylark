6afdc5d7f4ad4356086bf9e3003a03c87d8d22c4
Shockah <lolshockah@gmail.com>
2014-09-02 22:43:45 +0200

    SYoutube: vote percentage

--- SYoutube/src/syoutube/YoutubeURLAnnouncer.java
+++ SYoutube/src/syoutube/YoutubeURLAnnouncer.java
@@ -95,31 +95,38 @@ public class YoutubeURLAnnouncer extends URLAnnouncer {
 			return format(true);
 		}
 		public String format(boolean includeShortUrl) {
-			DecimalFormatSymbols symbols = DecimalFormatSymbols.getInstance();
-			symbols.setGroupingSeparator(',');
-			DecimalFormat formatter = new DecimalFormat("###,###", symbols);
-			StringBuilder sb = new StringBuilder();
-			
-			sb.append(String.format(" | &b%s&r", title));
-			sb.append(String.format(" | added by &b%s&r", uploader));
-			if (duration != 0) {
-				sb.append(String.format(" | %s long", TimeDuration.formatSeconds(duration)));
-			}
-			if (views != 0) {
-				sb.append(String.format(" | %s view%s", formatter.format(views), views == 1 ? "" : "s"));
-			}
-			if (voteUp + voteDown != 0) {
-				sb.append(String.format(" | +%s / -%s", formatter.format(voteUp), formatter.format(voteDown)));
-			}
-			
-			if (includeShortUrl) {
-				sb.append(String.format(" | http://youtu.be/%s", id));
-			}
-			
-			String ret = sb.toString().substring(3);
-			ret = ret.replace("&b", Colors.BOLD);
-			ret = ret.replace("&r", Colors.NORMAL);
-			return ret;
+			try {
+				DecimalFormatSymbols symbols = DecimalFormatSymbols.getInstance();
+				symbols.setGroupingSeparator(',');
+				DecimalFormat formatter = new DecimalFormat("###,###", symbols);
+				StringBuilder sb = new StringBuilder();
+				
+				sb.append(String.format(" | &b%s&r", title));
+				sb.append(String.format(" | added by &b%s&r", uploader));
+				if (duration != 0) {
+					sb.append(String.format(" | %s long", TimeDuration.formatSeconds(duration)));
+				}
+				if (views != 0) {
+					sb.append(String.format(" | %s view%s", formatter.format(views), views == 1 ? "" : "s"));
+				}
+				if (voteUp + voteDown != 0) {
+					DecimalFormatSymbols symbols2 = DecimalFormatSymbols.getInstance();
+					symbols2.setDecimalSeparator('.');
+					DecimalFormat formatter2 = new DecimalFormat("###.##", symbols2);
+					
+					sb.append(String.format(" | +%s / -%s (%s%%)", formatter.format(voteUp), formatter.format(voteDown), formatter2.format(100d * voteUp / (voteUp + voteDown))));
+				}
+				
+				if (includeShortUrl) {
+					sb.append(String.format(" | http://youtu.be/%s", id));
+				}
+				
+				String ret = sb.toString().substring(3);
+				ret = ret.replace("&b", Colors.BOLD);
+				ret = ret.replace("&r", Colors.NORMAL);
+				return ret;
+			} catch (Exception e) {e.printStackTrace();}
+			return "";
 		}
 	}
 }
