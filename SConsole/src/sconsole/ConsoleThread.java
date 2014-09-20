d04e4c9fe6f89d6adbf318835f47748ab68b02ce
Shockah <lolshockah@gmail.com>
2014-09-20 20:50:57 +0200

    SConsole: Just some extra try catches

--- SConsole/src/sconsole/ConsoleThread.java
+++ SConsole/src/sconsole/ConsoleThread.java
@@ -29,92 +29,95 @@ public class ConsoleThread extends Thread {
 	}
 	
 	public void run() {
-		screen = TerminalFacade.createScreen();
-		screen.startScreen();
-		if (screen.getTerminal() instanceof SwingTerminal) {
-			((SwingTerminal)screen.getTerminal()).getJFrame().addWindowListener(new WindowAdapter(){
-				public void windowClosing(WindowEvent e) {
-					plugin.botApp.stop();
+		try {
+			screen = TerminalFacade.createScreen();
+			screen.startScreen();
+			try {
+				if (screen.getTerminal() instanceof SwingTerminal) {
+					((SwingTerminal)screen.getTerminal()).getJFrame().addWindowListener(new WindowAdapter(){
+						public void windowClosing(WindowEvent e) {
+							plugin.botApp.stop();
+						}
+					});
 				}
-			});
-		}
-		rect = new ScreenRect(this);
-		borders = new Borders(rect);
-		
-		ConsoleViewSplitter cvs1 = new ConsoleViewSplitter(this);
-		//ConsoleViewSplitter cvs2 = new ConsoleViewSplitter(this);
-		ConsoleViewTabs cvtt = new ConsoleViewTabs(this);
-		ConsoleViewTab cvt = new ConsoleViewTab(this);
-		ConsoleViewOutput cvo = new ConsoleViewOutput(this);
-		
-		cvo.apply();
-		
-		view = cvs1;
-		cvs1.setMain(cvtt, ConsoleViewSplitter.Side.Top);
-		cvs1.setOff(cvt);
-		
-		cvtt.tabs.add(new ConsoleTab("Output", cvo));
-		
-		viewTab = cvtt.view = cvt;
-		viewTabs = cvt.view = cvtt;
-		
-		ScreenWriter sw = new ScreenWriter(screen);
-		synchronized (plugin.listeners) {for (IConsolePluginListener listener : plugin.listeners) {
-			listener.onConsoleEnabled();
-		}}
-		while (plugin.botApp.running && plugin.running) {
-			if (screen.resizePending()) {
-				screen.completeRefresh();
-				rect.setSizeToScreen();
+				rect = new ScreenRect(this);
 				borders = new Borders(rect);
-			}
-			
-			sw.setBackgroundColor(Color.BLACK);
-			sw.setForegroundColor(Color.WHITE);
-			sw.fillScreen(' ');
-			
-			if (view == null) {
-				setFocus(null);
-			} else {
-				if (focusStack.isEmpty()) {
-					if (view.focusable()) {
-						setFocus(view);
+				
+				ConsoleViewSplitter cvs1 = new ConsoleViewSplitter(this);
+				//ConsoleViewSplitter cvs2 = new ConsoleViewSplitter(this);
+				ConsoleViewTabs cvtt = new ConsoleViewTabs(this);
+				ConsoleViewTab cvt = new ConsoleViewTab(this);
+				ConsoleViewOutput cvo = new ConsoleViewOutput(this);
+				
+				cvo.apply();
+				try {
+					view = cvs1;
+					cvs1.setMain(cvtt, ConsoleViewSplitter.Side.Top);
+					cvs1.setOff(cvt);
+					
+					cvtt.tabs.add(new ConsoleTab("Output", cvo));
+					
+					viewTab = cvtt.view = cvt;
+					viewTabs = cvt.view = cvtt;
+					
+					ScreenWriter sw = new ScreenWriter(screen);
+					synchronized (plugin.listeners) {for (IConsolePluginListener listener : plugin.listeners) {
+						listener.onConsoleEnabled();
+					}}
+					while (plugin.botApp.running && plugin.running) {
+						if (screen.resizePending()) {
+							screen.completeRefresh();
+							rect.setSizeToScreen();
+							borders = new Borders(rect);
+						}
+						
+						sw.setBackgroundColor(Color.BLACK);
+						sw.setForegroundColor(Color.WHITE);
+						sw.fillScreen(' ');
+						
+						if (view == null) {
+							setFocus(null);
+						} else {
+							if (focusStack.isEmpty()) {
+								if (view.focusable()) {
+									setFocus(view);
+								}
+							} else {
+								while (!focusStack.isEmpty() && !focus().focusable()) {
+									popFocus();
+								}
+							}
+							view.rect.x = 0;
+							view.rect.y = 0;
+							view.rect.w = rect.w;
+							view.rect.h = rect.h;
+							view.update(null);
+							view.draw(null);
+						}
+						
+						borders.drawAndClear(rect);
+						if (cursor.getColumn() != -1 && cursor.getRow() != -1) {
+							screen.getTerminal().setCursorVisible(true);
+							screen.setCursorPosition(cursor);
+						}
+						screen.refresh();
+						if (cursor.getColumn() == -1 || cursor.getRow() == -1) {
+							screen.getTerminal().setCursorVisible(false);
+						} else {
+							screen.getTerminal().setCursorVisible(true);
+							cursor.setColumn(-1);
+							cursor.setRow(-1);
+						}
+						Util.sleep(50);
 					}
-				} else {
-					while (!focusStack.isEmpty() && !focus().focusable()) {
-						popFocus();
-					}
-				}
-				view.rect.x = 0;
-				view.rect.y = 0;
-				view.rect.w = rect.w;
-				view.rect.h = rect.h;
-				view.update(null);
-				view.draw(null);
-			}
-			
-			borders.drawAndClear(rect);
-			if (cursor.getColumn() != -1 && cursor.getRow() != -1) {
-				screen.getTerminal().setCursorVisible(true);
-				screen.setCursorPosition(cursor);
-			}
-			screen.refresh();
-			if (cursor.getColumn() == -1 || cursor.getRow() == -1) {
-				screen.getTerminal().setCursorVisible(false);
-			} else {
-				screen.getTerminal().setCursorVisible(true);
-				cursor.setColumn(-1);
-				cursor.setRow(-1);
-			}
-			Util.sleep(50);
-		}
-		synchronized (plugin.listeners) {for (IConsolePluginListener listener : plugin.listeners) {
-			listener.onConsoleDisabled();
-		}}
-		
-		cvo.restore();
-		
-		screen.stopScreen();
+					synchronized (plugin.listeners) {for (IConsolePluginListener listener : plugin.listeners) {
+						listener.onConsoleDisabled();
+					}}
+				} catch (Exception e) {e.printStackTrace();}
+				cvo.restore();
+			} catch (Exception e) {e.printStackTrace();}
+			screen.stopScreen();
+		} catch (Exception e) {e.printStackTrace();}
 		plugin.stopped = true;
 	}
 	
