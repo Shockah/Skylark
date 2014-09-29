ff17b5ae649db23803c25a2287facf9659b6c625
Michał Dolaś <michal.dolas@appdate.pl>
2014-09-29 09:46:26 +0200

    SWolframAlpha

--- SWolframAlpha/src/swolfram/CmdWolfram.java
+++ SWolframAlpha/src/swolfram/CmdWolfram.java
@@ -3,7 +3,9 @@ package swolfram;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.parser.Parser;
 import org.jsoup.select.Elements;
+import org.pircbotx.Colors;
 import scommands.Command;
 import shocky3.pircbotx.Bot;
 import shocky3.pircbotx.event.GenericUserMessageEvent;
@@ -27,7 +29,7 @@ public class CmdWolfram extends Command {
 				"appid", appid
 			);
 			if (req.ok()) {
-				Document doc = Jsoup.parse(req.body());
+				Document doc = Jsoup.parse(req.body(), "", Parser.xmlParser());
 				Elements qress = doc.select("queryresult");
 				if (qress.isEmpty()) {
 					e.respond("Failed.");
@@ -42,11 +44,36 @@ public class CmdWolfram extends Command {
 				
 				StringBuilder sb = new StringBuilder();
 				for (Element pod : qres.select("pod")) {
+					String id = pod.attr("id");
+					if (id.equals("VisualRepresentation")) continue;
+					if (id.equals("Illustration")) continue;
+					if (id.equals("Interpretation")) continue;
+					if (id.startsWith("ComparisonAs")) continue;
+					
 					for (Element subpod : pod.select("subpod")) {
 						Elements plaintexts = subpod.select("plaintext");
 						if (!plaintexts.isEmpty()) {
-							sb.append(" | ");
-							sb.append(plaintexts.get(0).val().trim());
+							String text = plaintexts.get(0).text().trim();
+							if (text.isEmpty()) continue;
+							if (text.indexOf('\n') != -1 || text.indexOf('\r') != -1) continue;
+							String title = pod.attr("title");
+							
+							if (id.equals("Result") || id.equals("Input")) {
+								title = String.format("%s%s%s", Colors.BOLD, title, Colors.NORMAL);
+								if (id.equals("Result")) {
+									text = String.format("%s%s%s", Colors.BOLD, text, Colors.NORMAL);
+								}
+							}
+							sb.append(String.format(" | %s: %s", title, text));
+						}
+					}
+					
+					if (id.equals("Input")) {
+						Elements assumptionss = qres.select("assumptions");
+						if (!assumptionss.isEmpty()) {
+							for (Element assumption : assumptionss.select("assumption")) {
+								sb.append(String.format(" | Assuming '%s' for '%s'", assumption.select("value").get(0).attr("desc"), assumption.attr("word")));
+							}
 						}
 					}
 				}
