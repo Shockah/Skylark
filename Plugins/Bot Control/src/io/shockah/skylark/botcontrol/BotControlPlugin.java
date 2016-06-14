b9479eafa27e676755816bf7c3073a81ed176012
Shockah <lolshockah@gmail.com>
2016-06-14 12:21:15 +0200

    [Bot Control] /invite handling

--- Plugins/Bot Control/src/io/shockah/skylark/botcontrol/BotControlPlugin.java
+++ Plugins/Bot Control/src/io/shockah/skylark/botcontrol/BotControlPlugin.java
@@ -1,11 +1,15 @@
 package io.shockah.skylark.botcontrol;
 
+import io.shockah.skylark.Bot;
+import io.shockah.skylark.BotManager;
 import io.shockah.skylark.commands.CommandsPlugin;
 import io.shockah.skylark.permissions.PermissionsPlugin;
-import io.shockah.skylark.plugin.Plugin;
+import io.shockah.skylark.plugin.ListenerPlugin;
 import io.shockah.skylark.plugin.PluginManager;
+import org.pircbotx.Channel;
+import org.pircbotx.hooks.events.InviteEvent;
 
-public class BotControlPlugin extends Plugin {
+public class BotControlPlugin extends ListenerPlugin {
 	@Dependency
 	protected CommandsPlugin commandsPlugin;
 	
@@ -30,5 +34,22 @@ public class BotControlPlugin extends Plugin {
 	
 	public JoinCommand getJoinCommand() {
 		return joinCommand;
+	}
+	
+	@Override
+	protected void onInvite(InviteEvent e) {
+		BotManager manager = e.<Bot>getBot().manager;
+		Bot foundBot = manager.bots.firstResult(bot -> {
+			for (Channel channel : bot.getUserBot().getChannels()) {
+				if (channel.getName().equals(e.getChannel()))
+					return bot;
+			}
+			return null;
+		});
+		
+		if (foundBot == null)
+			return;
+		
+		manager.joinChannel(e.getChannel());
 	}
 }
