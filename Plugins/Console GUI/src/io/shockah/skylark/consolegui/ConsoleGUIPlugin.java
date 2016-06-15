18dc228012205876c11ca1ef26abc9c1b2a21e15
Shockah <lolshockah@gmail.com>
2016-06-15 16:52:57 +0200

    [Console GUI] Empty project with lanterna dependency

---
+++ Plugins/Console GUI/src/io/shockah/skylark/consolegui/ConsoleGUIPlugin.java
@@ -0,0 +1,10 @@
+package io.shockah.skylark.consolegui;
+
+import io.shockah.skylark.plugin.Plugin;
+import io.shockah.skylark.plugin.PluginManager;
+
+public class ConsoleGUIPlugin extends Plugin {
+	public ConsoleGUIPlugin(PluginManager manager, Info info) {
+		super(manager, info);
+	}
+}
