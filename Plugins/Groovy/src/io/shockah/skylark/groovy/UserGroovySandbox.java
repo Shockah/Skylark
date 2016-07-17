bccd8d61d4e9a4773e9ccfe2ba0fa9cbe4de4b2e
Shockah <lolshockah@gmail.com>
2016-07-17 22:35:17 +0200

    [Groovy] More sandboxing

--- Plugins/Groovy/src/io/shockah/skylark/groovy/UserGroovySandbox.java
+++ Plugins/Groovy/src/io/shockah/skylark/groovy/UserGroovySandbox.java
@@ -10,31 +10,34 @@ import io.shockah.skylark.commands.CommandCall;
 public class UserGroovySandbox extends GroovySandbox {
 	public UserGroovySandbox() {
 		super();
-		addBlacklistedMethods(CommandCall.class, "respond");
 		
-		addWhitelistedMethods(PircBotX.class,
-				"getNick", "getServerHostname", "getServerPort", "getUserBot", "getUserChannelDao"
+		addBlacklistedMethods(CommandCall.class,
+				"respond"
 		);
-		
 		addBlacklistedMethods(Bot.class,
 				"getBotManager"
 		);
 		
+		addWhitelistedMethods(PircBotX.class,
+				"getNick", "getServerHostname", "getServerPort", "getUserBot", "getUserChannelDao"
+		);
 		addWhitelistedMethods(User.class,
 				"getAwayMessage", "getBot", "getHostmask", "getHostname", "getLogin", "getNick", "getRealName", "getServer",
 				"getChannels", "getChannelsHalfOpIn", "getChannelsOpIn", "getChannelsOwnerIn", "getChannelsSuperOpIn", "getChannelsVoiceIn",
 				"isAway", "isIrcop"
 		);
-		
 		addWhitelistedMethods(Channel.class,
 				"getBot", "getChannelLimit", "getMode", "getName", "getTopic", "getTopicSetter", "getTopicTimestamp",
 				"getHalfOps", "getNormalUsers", "getOps", "getOwners", "getSuperOps", "getUsers", "getUsersNicks", "getVoices",
 				"hasVoice", "isHalfOp", "isOp", "isOwner", "isSuperOp",
 				"hasTopicProtection", "isChannelPrivate", "isInviteOnly", "isModerated", "isNoExternalMessages", "isSecret"
 		);
-		
 		addWhitelistedMethods(UserHostmask.class,
 				"getHostmask", "getHostname", "getLogin", "getNick"
+		);
+		
+		addWhitelistedPackages(
+				"io.shockah.skylark.commands"
 		);
 	}
 }
