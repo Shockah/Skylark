df010ab54690b8b6450f57a37e95af6cca3eac85
Shockah <lolshockah@gmail.com>
2016-07-22 11:03:50 +0200

    [Commands] NamedCommand now requires at least one name

--- Plugins/Commands/src/io/shockah/skylark/commands/NamedCommand.java
+++ Plugins/Commands/src/io/shockah/skylark/commands/NamedCommand.java
@@ -3,7 +3,11 @@ package io.shockah.skylark.commands;
 public abstract class NamedCommand<T, R> extends Command<T, R> {
 	protected final String[] names;
 	
-	public NamedCommand(String... names) {
+	public NamedCommand(String name, String... extraNames) {
+		String[] names = new String[extraNames.length + 1];
+		names[0] = name;
+		for (int i = 0; i < extraNames.length; i++)
+			names[i + 1] = extraNames[i];
 		this.names = names;
 	}
 }
