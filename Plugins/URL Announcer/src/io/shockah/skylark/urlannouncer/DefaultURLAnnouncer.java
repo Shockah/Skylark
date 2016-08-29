2411ad095ddea7a0f97b3acb011e6b356b9c2b56
Shockah <lolshockah@gmail.com>
2016-08-29 16:02:49 +0200

    [URL Announcer] Unescaping HTML entities

--- Plugins/URL Announcer/src/io/shockah/skylark/urlannouncer/DefaultURLAnnouncer.java
+++ Plugins/URL Announcer/src/io/shockah/skylark/urlannouncer/DefaultURLAnnouncer.java
@@ -2,6 +2,7 @@ package io.shockah.skylark.urlannouncer;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.lang3.StringEscapeUtils;
 import com.github.kevinsawicki.http.HttpRequest;
 
 public class DefaultURLAnnouncer extends URLAnnouncer {
@@ -15,8 +16,11 @@ public class DefaultURLAnnouncer extends URLAnnouncer {
 			for (int i = 0; i < splitContentType.length; i++) splitContentType[i] = splitContentType[i].trim();
 			if (splitContentType[0].equals("text/html")) {
 				Matcher m = TITLE_PATTERN.matcher(req.body());
-				if (m.find())
-					return String.format("[%s]", m.group(1).replaceAll("\\s+", " ").trim());
+				if (m.find()) {
+					String title = m.group(1).replaceAll("\\s+", " ").trim();
+					title = StringEscapeUtils.unescapeHtml4(title);
+					return String.format("[%s]", title);
+				}
 			}
 		} catch (Exception e) {
 		}
